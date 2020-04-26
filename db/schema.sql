b4f6844916c5bf4a5f05ec007e05ff0d475fa409
Kate Jamboretz <kejamboretz@dons.usfca.edu>
2020-04-26 11:00:56 -0700

    set up burger database schema and seed files, initiate package.json and npm install express, express-handlebars, and mysql

---
+++ db/schema.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS burger_db;
+
+CREATE DATABASE burger_db;
+
+USE burger_db;
+
+CREATE TABLE burgers
+(
+    id INTEGER NOT NULL
+    AUTO_INCREMENT,
+    burger_name VARCHAR
+    (50) NOT NULL,
+    devoured BOOLEAN,
+    PRIMARY KEY
+    (id)
+);
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burger_db;
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ('Guaco-Heaven', true),
+    ('Spicy Chile', false),
+    ('Pineapple Craze', false);
